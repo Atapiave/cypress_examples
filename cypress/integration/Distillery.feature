0aa05f5e90cc3d9180ddba099b2c7cf8920cae06
Arturo Tapia <arturotapiav94@gmail.com>
2021-01-14 13:11:32 -0600

    Repo update

--- cypress/integration/Distillery.feature
+++ cypress/integration/Distillery.feature
@@ -1,8 +1,32 @@
 Feature: Distillery
 
-    Scenario: Main Page
+    # Scenario Outline: Main Page
+
+    #     Given we are in the Distillery main page
+    #     When we hover over the "<menu>" menu
+    #     And we select the "<option>" option
+    #     Then we verify we are in "<page>" page
+    #     And we scroll to "<point>" point
+
+    #     Examples:
+
+    #         | menu       | option                   | page                     | point                             |
+    #         | Industries | FinTech                  | FinTech                  | WHAT IS FINTECH?                  |
+    #         | Industries | Enterprise Cloud Storage | Enterprise Cloud Storage | WHAT IS ENTERPRISE CLOUD STORAGE? |
+    #         | Industries | Emerging Technologies    | Emerging Technologies    | WHAT IS EMERGING TECHNOLOGY?      |
+
+    Scenario Outline: Contact
 
         Given we are in the Distillery main page
-        When we select the "Industries" menu
-        And we select the "FinTech" option
-        Then we verify we are in "FinTech" page
+        When we select the "<menu>" menu
+        And we fill the fields on contact page with the next information
+            | name   | email   | phone   | info   | updates   | policy   |
+            | <name> | <email> | <phone> | <info> | <updates> | <policy> |
+        And we click on submit button
+        Then we assert that the message was sent
+
+
+        Examples:
+
+            | menu    | name      | email             | phone      | info                                       | updates | policy |
+            | Contact | QA test 1 | qatest1@gmail.com | 5511223344 | I'm an automation engineer from distillery | Y       | Y      |
--- cypress/integration/Distillery/Distillery.spec.js
+++ cypress/integration/Distillery/Distillery.spec.js
@@ -5,8 +5,12 @@ Given('we are in the Distillery main page', () => {
     cy.visit("https://distillery.com/")
 });
 
-When('we select the {string} menu', (menu_name) => {
+When('we hover over the {string} menu', (menu_name) => {
     ActionsPage.hover_element_from_list('.navigation-menu__item-link', menu_name)
+});
+
+When('we select the {string} menu', (menu_name) => {
+    ActionsPage.click_element_from_list('.navigation-menu__item-link', menu_name)
 });
 
 When('we select the {string} option', (option_name) => {
@@ -14,5 +18,32 @@ When('we select the {string} option', (option_name) => {
 });
 
 Then('we verify we are in {string} page', (page_title) => {
-    ActionsPage.assert_containts('.jumbotron__main-heading.styleguide-typography__main-heading', page_title)
+    ActionsPage.assert_contains('.jumbotron__main-heading.styleguide-typography__main-heading', page_title)
+});
+
+Then('we scroll to {string} point', (point) => {
+    ActionsPage.scroll_to_element('.expertise-detail__title.styleguide-typography__subtitle', point)
+});
+
+When('we fill the fields on contact page with the next information', (datatable) => {
+    datatable.hashes().forEach(element => {
+        ActionsPage.type_text('input#name_contact_page', element.name)
+        ActionsPage.type_text('input#email_contact_page', element.email)
+        ActionsPage.type_text('input#phone_contact_page', element.phone)
+        ActionsPage.type_text('#message_contact_page', element.info)
+        if (element.updates == 'Y') {
+            ActionsPage.click_element('.main-form-component__form-wrapper_contact .styleguide-checkbox__wrapper:nth-of-type(3) .styleguide-checkbox__checkbox')
+        }
+        if (element.policy == 'Y') {
+            ActionsPage.click_element('.main-form-component__form-wrapper_contact .styleguide-checkbox__wrapper:nth-of-type(4) .styleguide-checkbox__checkbox')
+        }
+    })
+});
+
+When('we click on submit button', ()  => {
+    ActionsPage.click_element_force('.main-form-component__form-wrapper_contact .main-form-component__form-button')
+});
+
+Then('we assert that the message was sent', () => {
+    ActionsPage.assert_contains('.contact__coll_form .main-form-component__successful-title', 'Your message was sent!')
 });
